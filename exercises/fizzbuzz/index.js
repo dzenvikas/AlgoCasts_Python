6a4d9aab2f33b9c01ef02811bcf283ce06b7b346
vikaskumar94 <vikaskumar.vsk@gmail.com>
2017-11-21 22:25:37 +0530

    solved in JavaScript

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,24 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {			// JavaScript takes n as integer...
+
+	// var a = parseInt(n);		//...so we don't need to parse n
+
+	for (var i = 1; i <= n;) {
+		if (i % 3 === 0 && i % 5 ===0) {
+			console.log('fizzbuzz')
+		} else if ( i % 3 === 0) {
+			console.log('fizz')
+		} else if ( i % 5 === 0){
+			console.log('buzz')
+		} else {
+			console.log(i)
+		}
+
+		i++;					// incerment count
+	}
+
+}
 
 module.exports = fizzBuzz;
